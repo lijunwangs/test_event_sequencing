4930f95a1192058bac1494b51ec2c80d4433e1e9
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-04-17 01:40:24 -0700

    make sync optional

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use crossbeam_channel::{unbounded, Receiver, Sender};
+use std::env;
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
+use std::time::Instant;
 
 #[derive(Debug, Clone)]
 struct Event {
@@ -8,51 +10,65 @@ struct Event {
     payload: String,
 }
 
-fn receiver_thread_a(name: &'static str, rx: Receiver<Event>, state: Arc<(Mutex<u64>, Condvar)>) {
+fn receiver_thread_a(
+    name: &'static str,
+    rx: Receiver<Event>,
+    state: Arc<(Mutex<u64>, Condvar)>,
+    use_sync: bool,
+) {
     for event in rx.iter() {
         let seq = event.seq;
 
         // It's our turn
-        println!("[Receiver {}] Processing {:?}", name, event);
+        // println!("[Receiver {}] Processing {:?}", name, event);
 
         // just notify it.
-        let mut guard = state.0.lock().unwrap();
-        *guard = seq;
-        // updating the last processed seq
-        println!(
-            "[Receiver {}] notify we are done with my event: {}, current: {}",
-            name, seq, *guard
-        );
-        state.1.notify_all();
+        if use_sync {
+            let mut guard = state.0.lock().unwrap();
+            *guard = seq;
+            // updating the last processed seq
+            // println!(
+            //     "[Receiver {}] notify we are done with my event: {}, current: {}",
+            //     name, seq, *guard
+            // );
+            state.1.notify_all();
+        }
     }
 
     println!("[Receiver {}] Done.", name);
 }
 
-fn receiver_thread_b(name: &'static str, rx: Receiver<Event>, state: Arc<(Mutex<u64>, Condvar)>) {
+fn receiver_thread_b(
+    name: &'static str,
+    rx: Receiver<Event>,
+    state: Arc<(Mutex<u64>, Condvar)>,
+    use_sync: bool,
+) {
     for event in rx.iter() {
         let seq = event.seq;
 
-        let mut guard = state.0.lock().unwrap();
+        if use_sync {
+            let mut guard = state.0.lock().unwrap();
 
-        // Wait until last_processed == seq - 1
-        while *guard < seq - 1 {
-            println!(
-                "[Receiver {}] my event: {} waiting for event {}, current: {}",
-                name,
-                seq,
-                seq - 1,
-                *guard
-            );
-            guard = state.1.wait(guard).unwrap();
-        }
+            // Wait until last_processed == seq - 1
+            while *guard < seq - 1 {
+                // println!(
+                //     "[Receiver {}] my event: {} waiting for event {}, current: {}",
+                //     name,
+                //     seq,
+                //     seq - 1,
+                //     *guard
+                // );
+                guard = state.1.wait(guard).unwrap();
+            }
 
-        // It's our turn
-        println!("[Receiver {}] Processing event {:?}", name, event);
-        if *guard < seq {
-            *guard = seq;
+            // It's our turn
+            // println!("[Receiver {}] Processing event {:?}", name, event);
+            if *guard < seq {
+                *guard = seq;
+            }
+            state.1.notify_all();
         }
-        state.1.notify_all();
     }
 
     println!("[Receiver {}] Done.", name);
@@ -61,7 +77,7 @@ fn receiver_thread_b(name: &'static str, rx: Receiver<Event>, state: Arc<(Mutex<
 fn sender_thread(tx_a: Sender<Event>, tx_b: Sender<Event>) {
     let mut seq = 1;
 
-    for i in 0..20 {
+    for i in 0..10000000 {
         let tx = if i % 2 == 0 { &tx_a } else { &tx_b };
 
         let event = Event {
@@ -69,11 +85,11 @@ fn sender_thread(tx_a: Sender<Event>, tx_b: Sender<Event>) {
             payload: format!("Payload {}", seq),
         };
 
-        println!(
-            "[Sender] Sending event {} to {}",
-            seq,
-            if i % 2 == 0 { "A" } else { "B" }
-        );
+        // println!(
+        //     "[Sender] Sending event {} to {}",
+        //     seq,
+        //     if i % 2 == 0 { "A" } else { "B" }
+        // );
 
         tx.send(event).unwrap();
         seq += 1;
@@ -86,6 +102,12 @@ fn sender_thread(tx_a: Sender<Event>, tx_b: Sender<Event>) {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let use_sync = args.get(1).map_or(false, |arg| arg == "sync");
+    println!("Using sync: {use_sync}");
+
+    let start = Instant::now();
+
     let (tx_a, rx_a) = unbounded::<Event>();
     let (tx_b, rx_b) = unbounded::<Event>();
 
@@ -94,11 +116,12 @@ fn main() {
     let state_a = Arc::clone(&shared_seq);
     let state_b = Arc::clone(&shared_seq);
 
-    let handle_a = thread::spawn(move || receiver_thread_a("A", rx_a, state_a));
-    let handle_b = thread::spawn(move || receiver_thread_b("B", rx_b, state_b));
+    let handle_a = thread::spawn(move || receiver_thread_a("A", rx_a, state_a, use_sync));
+    let handle_b = thread::spawn(move || receiver_thread_b("B", rx_b, state_b, use_sync));
     let sender_handle = thread::spawn(move || sender_thread(tx_a, tx_b));
 
     sender_handle.join().unwrap();
     handle_a.join().unwrap();
     handle_b.join().unwrap();
+    println!("All done in {}ms", start.elapsed().as_millis());
 }
